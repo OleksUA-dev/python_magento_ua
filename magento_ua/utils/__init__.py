ca749810058e5e7f62c82d5e10887dcf6caf8aea
Alex <dev@sometech.space>
2025-06-16 09:37:47 +0300

    init updated

--- magento_ua/utils/__init__.py
+++ magento_ua/utils/__init__.py
@@ -1,11 +1,100 @@
-"""Утилітарні функції для Magento бібліотеки."""
+"""
+Утилітарні функції для Magento бібліотеки.
+"""
 
-from .validation import ValidationError, validate_sku, validate_email, validate_phone
-from .serialization import JSONEncoder, JSONDecoder, serialize_model, deserialize_model
-from .helpers import format_price, parse_date, generate_url_key, clean_html
+from .validation import (
+    ValidationError,
+    validate_sku,
+    validate_email_address as validate_email,
+    validate_phone,
+    validate_price,
+    validate_weight,
+    validate_status,
+    validate_visibility,
+    validate_url_key,
+    validate_attribute_set_id,
+    validate_product_type,
+    validate_product_data,
+    validate_customer_data,
+    validate_search_criteria
+)
+
+from .serialization import (
+    JSONEncoder,
+    JSONDecoder,
+    serialize_to_json,
+    deserialize_from_json,
+    serialize_model,
+    deserialize_model,
+    serialize_to_binary,
+    deserialize_from_binary,
+    to_magento_format,
+    from_magento_format,
+    create_search_criteria
+)
+
+from .helpers import (
+    format_price,
+    parse_date,
+    generate_url_key,
+    clean_html,
+    safe_get,
+    generate_sku,
+    generate_random_string,
+    hash_string,
+    build_url,
+    is_valid_url,
+    chunk_list,
+    flatten_dict,
+    get_current_timestamp,
+    bytes_to_human_readable,
+    mask_sensitive_data
+)
 
 __all__ = [
-    'ValidationError', 'validate_sku', 'validate_email', 'validate_phone',
-    'JSONEncoder', 'JSONDecoder', 'serialize_model', 'deserialize_model',
-    'format_price', 'parse_date', 'generate_url_key', 'clean_html'
+    # Validation
+    'ValidationError',
+    'validate_sku',
+    'validate_email',
+    'validate_phone',
+    'validate_price',
+    'validate_weight',
+    'validate_status',
+    'validate_visibility',
+    'validate_url_key',
+    'validate_attribute_set_id',
+    'validate_product_type',
+    'validate_product_data',
+    'validate_customer_data',
+    'validate_search_criteria',
+
+    # Serialization
+    'JSONEncoder',
+    'JSONDecoder',
+    'serialize_to_json',
+    'deserialize_from_json',
+    'serialize_model',
+    'deserialize_model',
+    'serialize_to_binary',
+    'deserialize_from_binary',
+    'to_magento_format',
+    'from_magento_format',
+    'create_search_criteria',
+
+    # Helpers
+    'format_price',
+    'parse_date',
+    'generate_url_key',
+    'clean_html',
+    'safe_get',
+    'generate_sku',
+    'generate_random_string',
+    'hash_string',
+    'build_url',
+    'is_valid_url',
+    'chunk_list',
+    'flatten_dict',
+    'get_current_timestamp',
+    'bytes_to_human_readable',
+    'mask_sensitive_data'
 ]
